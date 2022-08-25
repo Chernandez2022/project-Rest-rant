ff8acc8cc839671c31fb87bbb400a395ada11513
Chernandez2022 <hcarlos0816@gmail.com>
2022-08-24 20:20:49 -0400

    added mongoose functionality

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -9,19 +9,24 @@ function new_form() {
         <form method="POST" action="/places">
           <div className="form-group">
             <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+            <input
+              className="form-control"
+              id="founded"
+              name="founded"
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" />
+            <input className="form-control" id="founded" name="founded" />
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" />
+            <input className="form-control" id="founded" name="founded" />
           </div>
           <div className="form-group">
             <label htmlFor="state">State</label>
-            <input className="form-control" id="state" name="state" />
+            <input className="form-control" id="founded" name="founded" />
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
